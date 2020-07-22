af8b8811c33d124f00840e2c8aad21681bb3fc07
levi <golevi666@gmail.com>
2020-07-22 17:33:39 +0800

    [add] add collect log lib

---
+++ lib/logger/logger.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+var (
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+func init() {
+	logPath := "log/errors.log"
+	errFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Open log file failed", err)
+	}
+
+	Info = log.New(os.Stdout, "Info:", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(os.Stdout, "Warning:", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(io.MultiWriter(os.Stderr, errFile), "Error:", log.Ldate|log.Ltime|log.Lshortfile)
+
+}
+
+func Println(v ...interface{}) {
+	Error.Println(v...)
+}
+
+func Printf(format string, v ...interface{}) {
+	Error.Printf(format, v...)
+}
