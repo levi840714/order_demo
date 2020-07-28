602c3bb65111f432f39918a9d1bd93c649c8d2dd
levi <golevi666@gmail.com>
2020-07-28 17:48:30 +0800

    [fix] modify order column goodsId

--- schema/order.go
+++ schema/order.go
@@ -4,8 +4,8 @@ import "time"
 
 type Order struct {
 	ID        int       `gorm:"column:id;AUTO_INCREMENT"`
-	AccountId string    `gorm:"column:accountId;type:varchar(12);NOT NULL"`
-	Goods     string    `gorm:"column:goods;type:varchar(20);NOT NULL"`
+	AccountId int       `gorm:"column:accountId;int;NOT NULL"`
+	GoodsId   int       `gorm:"column:goodsId;type:int;NOT NULL"`
 	Status    string    `gorm:"column:status;type:enum('0', '1');NOT NULL"`
 	OrderTime time.Time `gorm:"column:orderTime;type:datetime;NOT NULL"`
 }
